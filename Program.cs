64f2bd844d1e16c6f20480aa9192754f84f65639
Gordy <jordan.richards90@gmail.com>
2021-12-03 12:07:34 +0000

    Day3

--- Program.cs
+++ Program.cs
@@ -26,6 +26,9 @@ namespace AdventOfCode2021
                 case "2":
                     RunDay2();
                     break;
+                case "3":
+                    RunDay3();
+                    break;
             }
         }
 
@@ -117,6 +120,104 @@ namespace AdventOfCode2021
                 outPut += " (Aim : " + aim + ")";
 
             Console.Write(outPut);                
+        }
+
+        private static void RunDay3()
+        {
+            Console.WriteLine("Part 1 or Part 2?");
+
+            switch (Console.ReadLine())
+            {
+                case "2":
+                    RunDay3Part2();
+                    return;
+            }
+
+            var lines = ReadFileLineByLine(_folder + "Adv3.txt");
+            var maxLength = lines.FirstOrDefault().Length;
+            string gammaRateBinary = "";
+            string epsilonRateBinary = "";
+
+            for (int x = 0; x < maxLength; x++)
+            {
+                gammaRateBinary += GetCommonBit(lines, x);
+                epsilonRateBinary += GetCommonBit(lines, x, false);
+            }
+            
+            var gammaRate = Convert.ToInt32(gammaRateBinary, 2);
+            var epsilonRate = Convert.ToInt32(epsilonRateBinary, 2);
+
+            var output = string.Format("Gamma Rate: {0} Dec: {1}, Epsilon Rate: {2} Dec: {3}, Result: {4}", 
+                                                gammaRateBinary, gammaRate, epsilonRateBinary, epsilonRate, gammaRate * epsilonRate);
+
+            Console.Write(output);
+        }
+
+        private static void RunDay3Part2()
+        {
+            List<string> lines = ReadFileLineByLine(_folder + "Adv3.txt");
+
+            var oxygenRatingBinary = GetReducingListCommonBit(lines);
+            var coScrubberRatingBinary = GetReducingListCommonBit(lines, false);
+
+            var oxygenGenRating = Convert.ToInt32(oxygenRatingBinary, 2);
+            var coScrubberRating = Convert.ToInt32(coScrubberRatingBinary, 2);
+
+            var output = string.Format("Oxygen Generator Rating: {0} Dec: {1}, CO2 Scrubber Rating: {2} Dec: {3}, Result = {4}",
+                                                oxygenRatingBinary, oxygenGenRating, coScrubberRatingBinary, coScrubberRating, oxygenGenRating * coScrubberRating);
+
+            Console.Write(output);
+        }
+
+        private static string GetCommonBit(List<string>lines, int stringIndex, bool mostCommon = true)
+        {
+            int zeroBits = 0;
+            int oneBits = 0;
+
+            for (int i = 0; i < lines.Count; i++)
+            {
+                var line = lines[i][stringIndex];
+
+                if (line.Equals('0'))
+                    zeroBits++;
+                else if (line.Equals('1'))
+                    oneBits++;
+            }
+
+            if (zeroBits > oneBits)
+                return mostCommon ? "0" : "1";
+            if (oneBits > zeroBits)
+                return mostCommon ? "1" : "0";
+
+            //Equally common
+            return mostCommon ? "1" : "0";
+        }
+
+        private static string GetReducingListCommonBit(List<String> lines, bool mostCommon = true)
+        {
+            string candidate = "";
+            int currentStringIndex = 0;
+            List<string> candidates = lines;
+            List<string> newCandidates = new List<string>();
+
+            while (newCandidates.Count != 1)
+            {
+                newCandidates = new List<string>();
+
+                candidate += GetCommonBit(candidates, currentStringIndex, mostCommon);
+
+                for (int i = 0; i < candidates.Count; i++)
+                {
+                    if (candidates[i].StartsWith(candidate))
+                        newCandidates.Add(candidates[i]);
+                }
+
+                candidates = newCandidates;
+
+                currentStringIndex++;
+            }
+
+            return newCandidates.First();
         }
     }
 }
